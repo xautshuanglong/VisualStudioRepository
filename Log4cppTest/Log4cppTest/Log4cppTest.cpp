2f84baeb1a83e7df7854fdeb8736a42011a52e12
xautshuanglong <xjshuanglong@126.com>
2016-03-11 14:31:58 +0800

    modify layoutPattern paramter

--- Log4cppTest/Log4cppTest/Log4cppTest.cpp
+++ Log4cppTest/Log4cppTest/Log4cppTest.cpp
@@ -14,12 +14,13 @@ int main()
 	log4cpp::Category::getRoot().getAppender()->setLayout(layout);
 	log4cpp::Category& cat = log4cpp::Category::getInstance("cat1");
 
-	layout->setConversionPattern("%d{%Y-%m-%d %H:%M:%S.%l} [%p] %c %m%n");
+	layout->setConversionPattern("%d{%Y-%m-%d %H:%M:%S.%l} [%-5p] %c %m%n");
 	cat.setPriority(log4cpp::Priority::DEBUG);
 	cat.info("ha ha");
 	cat.debug("debug Testing");
 	cat.error("error Testing");
 	cat.fatal("fatal Testing");
+	cat.warn(string("warn Testing"));
 
 	log4cpp::Category::shutdown();
 

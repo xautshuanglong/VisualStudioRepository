a2384dbab1cfe47f7474b8a2ef25de10e68c2d20
xautshuanglong <xjshuanglong@126.com>
2016-03-20 15:45:58 +0800

    change project directory struction

--- Log4cppTest/Log4cppTest/Log4cppTest.cpp
+++ Log4cppTest/Log4cppTest/Log4cppTest.cpp
@@ -2,21 +2,43 @@
 #include <log4cpp/BasicLayout.hh>
 #include <log4cpp/BasicConfigurator.hh>
 #include <log4cpp/Category.hh>
+#include <log4cpp/FileAppender.hh>
 #include <log4cpp/PatternLayout.hh>
+#include <log4cpp/LayoutAppender.hh>
+#include <log4cpp/RollingFileAppender.hh>
 
 using namespace std;
 
 int main()
 {
-	cout << "Log4cppTest" << endl;
 	log4cpp::BasicConfigurator::configure();
-	log4cpp::PatternLayout* layout = new log4cpp::PatternLayout();
-	log4cpp::Category::getRoot().getAppender()->setLayout(layout);
-	log4cpp::Category& cat = log4cpp::Category::getInstance("cat1");
 
-	layout->setConversionPattern("%d{%Y-%m-%d %H:%M:%S.%l} [%-5p] %c %m%n");
+	// Every appender have a mutual independent layout.
+	log4cpp::PatternLayout* consoleLayout = new log4cpp::PatternLayout();
+	log4cpp::PatternLayout* fileLayout = new log4cpp::PatternLayout();
+	log4cpp::PatternLayout* rollLayout = new log4cpp::PatternLayout();
+	consoleLayout->setConversionPattern("%d{%H:%M:%S.%l} [%-5p] %c %m%n");
+	fileLayout->setConversionPattern("%d{%Y-%m-%d %H:%M:%S.%l} [%-5p] %c %m%n");
+	rollLayout->setConversionPattern("%d{%Y-%m-%d %H:%M:%S.%l} [%-5p] %c %m%n");
+	
+	// default appender, is equivalent to ostreamAppender with std::cout.
+	log4cpp::Category& root = log4cpp::Category::getRoot();
+	root.getAppender()->setLayout(consoleLayout);
+
+	// FileAppender testing.
+	log4cpp::FileAppender* fileAppender = new log4cpp::FileAppender("fileAppender", "..//logs//TempDebug.log",false);
+	fileAppender->setLayout(fileLayout);
+	root.addAppender(fileAppender);
+
+	// RollingFileAppender testing.
+	log4cpp::RollingFileAppender* rollingApender = new log4cpp::RollingFileAppender("rollingAppender", "../logs//RollingFile.log",1024*1024,10);
+	rollingApender->setLayout(rollLayout);
+	root.addAppender(rollingApender);
+
+	log4cpp::Category& cat = root.getInstance("Log4cppTest");
+
 	cat.setPriority(log4cpp::Priority::DEBUG);
-	cat.info("ha ha");
+	cat.info("info Testing");
 	cat.debug("debug Testing");
 	cat.error("error Testing");
 	cat.fatal("fatal Testing");

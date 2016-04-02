9b71a352211dbd413d5280240863158523bca6cb
xautshuanglong <xjshuanglong@126.com>
2016-04-02 18:25:13 +0800

    M priority is property of category，divide tempfile and rollingfile into different category

--- Log4cppTest/Log4cppConfigTest/Log4cppConfigTest.cpp
+++ Log4cppTest/Log4cppConfigTest/Log4cppConfigTest.cpp
@@ -23,21 +23,20 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 
-	Category& root = Category::getRoot();
-	Category& sub1 = Category::getInstance("file");
+	Category& sub1 = Category::getInstance("tempfile");
 	Category& sub2 = Category::getInstance("rolling");
 
 	NDC::push("Log4cppConfigTest");
 
 	sub1.info("sub1 info testing");
-	sub1.debug("sub1 info testing");
-	sub1.warn("sub1 info testing");
-	sub1.error("sub1 info testing");
+	sub1.debug("sub1 debug testing");
+	sub1.warn("sub1 warn testing");
+	sub1.error("sub1 error testing");
 
-	sub2.info("sub2 alert testing");
-	sub2.debug("sub1 info testing");
-	sub2.warn("sub1 info testing");
-	sub2.error("sub1 info testing");
+	sub2.info("sub2 info testing");
+	sub2.debug("sub2 debug testing");
+	sub2.warn("sub2 warn testing");
+	sub2.error("sub2 error testing");
 
 	return 0;
 }

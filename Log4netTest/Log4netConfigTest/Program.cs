fdfe5e2c2694bbe4aa9ca3ebce720f8336a095c5
Shuanglong <xjshuanglong@126.com>
2016-08-12 17:27:53 +0800

    control log4net level when release application

--- Log4netTest/Log4netConfigTest/Program.cs
+++ Log4netTest/Log4netConfigTest/Program.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using log4net;
+using log4net.Appender;
 using log4net.Config;
+using log4net.Core;
+using log4net.Filter;
 
 //[assembly: XmlConfigurator(Watch = true)]
 [assembly: XmlConfigurator(ConfigFile = @"..\\..\\Log4netConfig.xml", Watch = true)]
@@ -17,9 +20,36 @@ namespace Log4netConfigTest
 		static void Main(string[] args)
 		{
 			ILog log = LogManager.GetLogger("root");
+#if !DEBUG
+			IAppender[] appenders = log.Logger.Repository.GetAppenders();
 
-			log.Debug(log.Logger.Name);
-			
+			LevelRangeFilter levelRange = new LevelRangeFilter();
+			levelRange.LevelMin = Level.Info;
+			levelRange.LevelMax = Level.Fatal;
+
+			foreach (IAppender appenderItem in appenders)
+			{
+				if (appenderItem is ConsoleAppender)
+				{
+					ConsoleAppender tempAppender = (ConsoleAppender)appenderItem;
+					tempAppender.ClearFilters();
+					tempAppender.AddFilter(levelRange);
+				}
+				else if (appenderItem is FileAppender)
+				{
+					FileAppender tempAppender = (FileAppender)appenderItem;
+					tempAppender.ClearFilters();
+					tempAppender.AddFilter(levelRange);
+				}
+				else if (appenderItem is RollingFileAppender)
+				{
+					RollingFileAppender tempAppender = (RollingFileAppender)appenderItem;
+					tempAppender.ClearFilters();
+					tempAppender.AddFilter(levelRange);
+				}
+			}
+#endif
+
 			log.Debug("Debug Testing...");
 			log.Warn("Warn Testing ...");
 			log.Info("Info Testing ...");

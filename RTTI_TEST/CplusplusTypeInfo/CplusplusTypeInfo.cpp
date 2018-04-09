7960491c9de61a9db9a78ff9da3f8785f451ef90
xautshuanglong <xjshuanglong@126.com>
2018-04-09 19:33:05 +0800

    RTTI modify

--- RTTI_TEST/CplusplusTypeInfo/CplusplusTypeInfo.cpp
+++ RTTI_TEST/CplusplusTypeInfo/CplusplusTypeInfo.cpp
@@ -5,55 +5,69 @@ using namespace std;
 class BaseClass
 {
 public:
-	int iValue;
-	BaseClass() { cout << "BaseClass is constructed..." << endl; }
-	void Print() { cout << "BaseClass --> Print ..." << endl; }
+    int iValue;
+    BaseClass()
+    {
+        cout << "BaseClass is constructed..." << endl;
+    }
+    virtual void Print()
+    {
+        cout << "BaseClass --> Print ..." << endl;
+    }
 };
 
 class DeriveClass :public BaseClass
 {
 public:
-	float fValue;
-	DeriveClass() { cout << "DeriveClass is constructed..." << endl; }
-	virtual void Print() { cout << "DeriveClass --> Print ..." << endl; }
+    float fValue;
+    DeriveClass()
+    {
+        cout << "DeriveClass is constructed..." << endl;
+    }
+    void Print()
+    {
+        cout << "DeriveClass --> Print ..." << endl;
+    }
 };
 
 void CheckObjType(BaseClass* pObj);
 
 int main(int argc, char **argv)
 {
-	cout << "=====================  typeid erros  ====================" << endl;
+    cout << "=====================  typeid erros  ====================" << endl;
 
-	BaseClass bc;
-	DeriveClass dc;
+    BaseClass bc;
+    DeriveClass dc;
 
-	BaseClass *pBaseDerive = new DeriveClass();
+    BaseClass *pBaseDerive = new DeriveClass();
 
-	CheckObjType(&bc);
-	CheckObjType(&dc);
-	CheckObjType(pBaseDerive);
+    CheckObjType(&bc);
+    CheckObjType(&dc);
+    CheckObjType(pBaseDerive);
 
-	return 0;
+    return 0;
 }
 
 void CheckObjType(BaseClass* pObj)
 {
-	if (typeid(*pObj) == typeid(DeriveClass))
-	{
-		//DeriveClass *pDerive = dynamic_cast<DeriveClass*>(pObj);
-		//pDerive->Print();
-		cout << "Bingo DeriveClass..." << endl;
-	}
-	else if (typeid(*pObj) == typeid(BaseClass))
-	{
-		BaseClass *pBase = dynamic_cast<BaseClass*>(pObj);
-		pBase->Print();
+    // 存在多态（polymorphic）方可成功
+    // 若 BaseClass 中不存在 virtual function，测试将失败（没有多态），甚至编译不通过。
+    if (typeid(*pObj) == typeid(DeriveClass))
+    {
+        DeriveClass *pDerive = dynamic_cast<DeriveClass*>(pObj);
+        pDerive->Print();
+        cout << "Bingo DeriveClass... name=" << typeid(pObj).name() << "   *name=" << typeid(*pObj).name() << endl;
+    }
+    else if (typeid(*pObj) == typeid(BaseClass))
+    {
+        BaseClass *pBase = dynamic_cast<BaseClass*>(pObj);
+        pBase->Print();
 
-		cout << "Bingo BaseClass..." << endl;
-	}
-	else
-	{
-		cout << "unknown type --> " << typeid(pObj).name() << endl;
-		cout << "unknown type --> " << typeid(*pObj).name() << endl;
-	}
+        cout << "Bingo BaseClass... name=" << typeid(pObj).name() << "   *name=" << typeid(*pObj).name() << endl;
+    }
+    else
+    {
+        cout << "unknown type --> " << typeid(pObj).name() << endl;
+        cout << "unknown type --> " << typeid(*pObj).name() << endl;
+    }
 }

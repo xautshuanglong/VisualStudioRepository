130a035de356f5455cf16939fb63510b1c5f6df1
xautshuanglong <xjshuanglong@126.com>
2018-04-09 18:39:22 +0800

    csharp is keyword test

--- RTTI_TEST/CsharpRTTI/Program.cs
+++ RTTI_TEST/CsharpRTTI/Program.cs
@@ -6,11 +6,42 @@ using System.Threading.Tasks;
 
 namespace CsharpRTTI
 {
+    class BaseClass
+    {
+        public void Test() { Console.WriteLine("BaseClass::Test"); }
+        public virtual void VirtualFunc() { Console.WriteLine("BaseClass::VirtualFunc"); }
+    }
+
+    class DerivedClass : BaseClass
+    {
+        public new void Test() { Console.WriteLine("DerivedClass::Test"); }
+        public override void VirtualFunc() { Console.WriteLine("DerivedClass::VirtualFunc"); }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("========================= RTTI Testing ========================");
+
+            BaseClass bc = new BaseClass();
+            DerivedClass dc = new DerivedClass();
+
+            bc.Test();
+            bc.VirtualFunc();
+            Console.Write("bc is BaseClass? {0}  bc is DerivedClass? {1}", bc is BaseClass, bc is DerivedClass);
+            Console.Write(Environment.NewLine);
+
+            dc.Test();
+            dc.VirtualFunc();
+            Console.Write("dc is BaseClass? {0}  dc is DerivedClass? {1}", dc is BaseClass, dc is DerivedClass);
+            Console.Write(Environment.NewLine);
+
+            BaseClass bcdc = new DerivedClass();
+            bcdc.Test();
+            bcdc.VirtualFunc();
+            Console.Write("bcdc is BaseClass? {0}  bcdc is DerivedClass? {1}", bcdc is BaseClass, bcdc is DerivedClass);
+            Console.Write(Environment.NewLine);
         }
     }
 }
